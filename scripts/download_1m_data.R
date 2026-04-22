3632dd8627da2724f7f73316254cdb30a0b1b588
victorverma <vishashv@gmail.com>
2026-04-21 20:13:33 -0400

    Update links for GOES data files

--- scripts/download_1m_data.R
+++ scripts/download_1m_data.R
@@ -30,12 +30,12 @@ urls <- c(
   goes10 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes10/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g10_s19980701_e20091201_v1-0-0.nc",
   goes11 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes11/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g11_s20060601_e20080210_v1-0-0.nc",
   goes12 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes12/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g12_s20030110_e20070412_v1-0-0.nc",
-  goes13 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes13/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g13_s20130607_e20171214_v2-2-0.nc",
-  goes14 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes14/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g14_s20090919_e20200304_v2-2-0.nc",
-  goes15 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes15/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g15_s20100407_e20200304_v2-2-0.nc",
-  goes16 = "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes16/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g16_s20170207_e20241122_v2-2-0.nc",
-  goes17 = "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes17/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g17_s20180601_e20230110_v2-2-0.nc",
-  goes18 = "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes18/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g18_s20220902_e20241122_v2-2-0.nc"
+  goes13 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes13/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g13_s20130607_e20171214_v2-2-1.nc",
+  goes14 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes14/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g14_s20090919_e20200304_v2-2-1.nc",
+  goes15 = "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes15/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g15_s20100407_e20200304_v2-2-1.nc",
+  goes16 = "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes16/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g16_s20170207_e20250406_v2-2-1.nc",
+  goes17 = "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes17/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g17_s20180601_e20230110_v2-2-1.nc",
+  goes18 = "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes18/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g18_s20220617_e20260420_v2-2-1.nc"
 )
 
 mcmapply(download_data, urls, names(urls), mc.cores = detectCores()) %>%
